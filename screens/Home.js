9e85dbdb917e51abe5b5dfcf18a7d119340c515f
ali-hamidaddin <ali.hamidaddin@generalassemb.ly>
2021-01-11 11:48:04 +0300

    setup files

---
+++ screens/Home.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
+
+const Home = () => {
+
+    useEffect(() => {
+        //get posts
+    })
+
+    return <SafeAreaView>
+        <ScrollView>
+            
+        </ScrollView>
+    </SafeAreaView>
+}
+
+const styles = StyleSheet.create({
+    // add styles
+});
+
+export default Home;
---
+++ screens/Post.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { SafeAreaView, ScrollView } from 'react-native';
+
+const Post = () => {
+
+    useEffect(() => {
+        //get posts
+    })
+
+    return <SafeAreaView>
+        <ScrollView>
+            
+        </ScrollView>
+    </SafeAreaView>
+}
+
+const styles = StyleSheet.create({
+    // add styles
+});
+
+export default Post;
